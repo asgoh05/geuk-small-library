e67ba5893082c99ed33fc0c7c65c357012aeea2a
asgoh05 <asgoh05@gmail.com>
2024-04-25 17:11:02 +0900

    fix#3 fix admin password logic

--- app/admin/components/AdminNav.tsx
+++ app/admin/components/AdminNav.tsx
@@ -1,42 +1,37 @@
 "use client";
 import Link from "next/link";
-import AdminPassword from "./AdminPassword";
+import AdminPasswordWrap from "./AdminPasswordWrap";
 import { useState } from "react";
 
 export default function AdminNav() {
-  const [verified, setVerified] = useState(false);
   return (
     <div>
-      {verified ? (
-        <div className="flex gap-4 justify-center">
-          <Link
-            className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
-            href="/admin"
-          >
-            Admin Home
-          </Link>
-          <Link
-            className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
-            href="/admin/addbook"
-          >
-            도서 추가
-          </Link>
-          <Link
-            className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
-            href="/admin/addbookExcel"
-          >
-            도서 추가 (Excel)
-          </Link>
-          <Link
-            className="rounded-full text-sm px-2 py-1 bg-red-200 hover:opacity-50"
-            href="/"
-          >
-            Close
-          </Link>
-        </div>
-      ) : (
-        <AdminPassword onVerified={() => setVerified(true)} />
-      )}
+      <div className="flex gap-4 justify-center">
+        <Link
+          className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
+          href="/admin"
+        >
+          Admin Home
+        </Link>
+        <Link
+          className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
+          href="/admin/addbook"
+        >
+          도서 추가
+        </Link>
+        <Link
+          className="rounded-full text-sm px-2 py-1 border hover:opacity-50"
+          href="/admin/addbookExcel"
+        >
+          도서 추가 (Excel)
+        </Link>
+        <Link
+          className="rounded-full text-sm px-2 py-1 bg-red-200 hover:opacity-50"
+          href="/"
+        >
+          Close
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ app/admin/components/AdminPasswordWrap.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { BaseSyntheticEvent, useState, useCallback } from "react";
+import { IoReturnDownBackOutline } from "react-icons/io5";
+
+export default function AdminPasswordWrap({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isVerified, setIsVerified] = useState(false);
+  const callbackRef = useCallback((inputElement: HTMLInputElement) => {
+    if (inputElement) {
+      inputElement.focus();
+    }
+  }, []);
+
+  const changePassword = (e: BaseSyntheticEvent) => {
+    setPassword(e.target.value);
+  };
+  const verifyPassword = (e: BaseSyntheticEvent) => {
+    e.preventDefault();
+    if (password === process.env.NEXT_PUBLIC_APP_ADM_PASSWORD) {
+      setIsVerified(true);
+    } else {
+      setMessage("wrong password");
+      setPassword("");
+      setTimeout(() => setMessage(""), 3 * 1000);
+    }
+  };
+  return (
+    <div>
+      {isVerified ? (
+        <div>{children}</div>
+      ) : (
+        <div className="fixed top-0 left-0 w-full h-full backdrop-blur-xl flex flex-col justify-center items-center z-10">
+          <form onSubmit={verifyPassword}>
+            <div className="pb-2">
+              <p>Admin Password:</p>
+            </div>
+            <div className="flex gap-4">
+              <input
+                className="rounded-full px-2 py-1 border"
+                type="password"
+                onChange={changePassword}
+                value={password}
+                ref={callbackRef}
+              />
+              <button type="submit" className="relative right-10 font-bold">
+                <IoReturnDownBackOutline />
+              </button>
+            </div>
+          </form>
+          <div>
+            <p className="text-xs text-red-500 relative right-4">{message}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
